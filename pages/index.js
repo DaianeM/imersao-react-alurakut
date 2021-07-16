6313535c1a956da04a2fb15bd999453f580fc333
DaianeM <daianersm2@gmail.com>
2021-07-16 17:42:54 -0300

    authenticated

--- pages/index.js
+++ pages/index.js
@@ -165,6 +165,13 @@ export async function getServerSideProps(context){
   const cookies = nookies.get(context);  
   const token = cookies.USER_TOKEN ;
 
+  const { githubUser } = jwt.decode(token);
+  return{
+    props: {
+      githubUser
+    }
+  }
+
   /*const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
     headers: {
         Authorization: token
@@ -181,12 +188,5 @@ export async function getServerSideProps(context){
       }
     }
   }*/
-
-  const { githubUser } = jwt.decode(token);
-  return{
-    props: {
-      githubUser
-    }
-  }
 
 }
